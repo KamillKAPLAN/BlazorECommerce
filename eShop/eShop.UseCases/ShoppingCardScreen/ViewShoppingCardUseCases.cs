f3c0a59ce44bdeed72effe0fd3a18ac9c3fe515c
KamillKAPLAN <kamilkaplnn@gmail.com>
2021-04-12 18:10:22 +0300

    5. Build Customer Portal - View Shopping Cart Use Case

---
+++ eShop/eShop.UseCases/ShoppingCardScreen/ViewShoppingCardUseCases.cs
@@ -0,0 +1,21 @@
+﻿using eShop.CoreBusiness.Models;
+using eShop.UseCases.PluginInterfaces.UI;
+using System.Threading.Tasks;
+
+namespace eShop.UseCases.ShoppingCardScreen
+{
+    public class ViewShoppingCardUseCases
+    {
+        private readonly IShoppingCart shoppingCard;
+
+        public ViewShoppingCardUseCases(IShoppingCart shoppingCard)
+        {
+            this.shoppingCard = shoppingCard;
+        }
+
+        public Task<Order> Execute()
+        {
+            return shoppingCard.GetOrderAsync();
+        }
+    }
+}
